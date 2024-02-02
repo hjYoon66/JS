56f303417ffd6dddfa494729ac8a43ebb433ffef
hjYoon66 <poiu601@naver.com>
2024-02-02 17:51:05 +0900

    JS MDN Study :running:

---
+++ JSMDN/array.js
@@ -0,0 +1,51 @@
+/**
+ * 배열
+ * 요소가 하나인 배열을 만들기 : Array.of
+ * length : 배열의 길이는 항상 마지막 요소의 인덱스에 1에 더한 값을 반환
+ *
+ */
+
+// const arrayLength = 3;
+// const arr1 = new Array(arrayLength);
+// arr1
+// const arr2 = Array(arrayLength)
+// arr2
+// const arr3 = [];
+// arr3.length = arrayLength;
+// arr3
+
+// const arr1 = [42];
+// arr1
+// const arr2 = Array(42)
+// arr2
+// const arr3 = [];
+// arr3.length = 42;
+// arr3
+//
+// const wisenArray = Array.of(9, 3);
+// wisenArray
+
+// const myArray = ["Wind", "Rain", "Fire"];
+// console.log(myArray[2])
+// console.log(myArray.length)
+
+// const emp = [];
+// emp[0] = "Jones";
+// emp[1] = "Lesh";
+// emp[2] = "West"
+// emp
+
+// const cats = [];
+// cats[30] = ["Dusty"];
+// console.log(cats.length); // 31
+
+// const colors = ['red','green', 'blue'];
+// colors.forEach((color) => console.log(color))
+
+// let myArray = ['1', '2', '3'];
+// myArray = myArray.concat('a','b','c')
+// myArray
+
+// const myArray = ['Wind', 'Rain', 'Fire'];
+// const list = myArray.join(" - ");
+// list
