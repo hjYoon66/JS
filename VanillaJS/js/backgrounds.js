c892fc77a161b6fb89ebbf6830216bc057d4c6ac
hjYoon66 <poiu601@naver.com>
2024-02-07 14:48:49 +0900

    VanilaJS Weather&CSS work :alien:

--- VanillaJS/js/backgrounds.js
+++ VanillaJS/js/backgrounds.js
@@ -1,9 +1,22 @@
-const images = ["img1.jpg", "img2.jpg", "img3.jpg"]
+const images = ["0.jpg", "1.jpg", "2.jpg"]
 
-const chosenImgae = images[Math.floor(Math.random() * images.length)]
+const body = document.querySelector("body");
 
-const bgImage = document.createElement("img")
+function paintImage(chosenImage) {
+    const image = new Image();
+    image.src = `img/${chosenImage}`; // 가져올 image경로 지정
+    console.log(image.src)
+    image.classList.add("bgImage"); // image에 bgImage 클래스 추가
+    body.appendChild(image); // body의 자식에 image추가
+}
 
-bgImage.src = `img/${chosenImgae}`
+function genRandom() {
+    const chosenImage = images[Math.floor(Math.random() * images.length)];
+    return chosenImage;
+}
 
-document.body.appendChild(bgImage)
+function init() {
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+init();
