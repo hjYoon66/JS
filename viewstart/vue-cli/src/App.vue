b2a29815a7d1b85a72286bdef4393da925db4a20
hjYoon66 <poiu601@naver.com>
2024-02-13 15:12:03 +0900

    Vue Component :satisfied:

--- viewstart/vue-cli/src/App.vue
+++ viewstart/vue-cli/src/App.vue
@@ -1,29 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <AppHeader
+        v-bind:propsdata="str"
+        v-on:renew="renewStr"></AppHeader>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from "./components/AppHeader.vue";
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      str: 'Header'
+    }
+  },
+  components:{
+    AppHeader,
+  },
+  methods:{
+    renewStr:function(){
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
