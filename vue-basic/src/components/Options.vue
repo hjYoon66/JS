77b92453e78214ff020ffb04a9d2383f54786f91
hjYoon66 <poiu601@naver.com>
2024-02-14 14:57:13 +0900

    Vue Basic :exclamation:

---
+++ vue-basic/src/components/Options.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
+  mounted() {
+    console.log(`숫자 세기의 초기값은 ${ this.count } 입니다.`)
+  }
+}
+</script>
+
+<template>
+  <button @click="increment">숫자 세기: {{ count }}</button>
+</template>
---
+++ vue-basic/src/components/SFC.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="count++">Count is: {{count}}</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<style scoped>
+button{
+  font-weight: bold;
+}
+</style>
