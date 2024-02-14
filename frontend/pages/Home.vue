afefbbd881622908c1b30f14d1f173241533048c
hjYoon66 <poiu601@naver.com>
2024-02-14 16:23:34 +0900

    Gallery Shoppingmall Backend work :heart:

--- frontend/pages/Home.vue
+++ frontend/pages/Home.vue
@@ -17,8 +17,8 @@
     <div class="album py-5 bg-body-tertiary">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="i in 12" :key="i">
-            <Card />
+          <div class="col" v-for="(item, idx) in state.items" :key="idx">
+            <Card :item="item"/>
           </div>
         </div>
       </div>
@@ -27,10 +27,21 @@
 </template>
 <script>
 import Card from "@/components/Card.vue";
+import axios from "axios";
+import {reactive} from "vue";
 
 export default {
   name: 'Home',
-  components: {Card}
+  components: {Card},
+  setup(){
+    const state = reactive({
+      items: []
+    })
+    axios.get("/api/items").then(({data})=>{
+      state.items = data
+    })
+    return {state}
+  }
 }
 </script>
 <style scoped>
