2e24222f6aee470be220975a4c186e06cb59c6ea
hjYoon66 <poiu601@naver.com>
2024-02-19 10:56:40 +0900

    Gallery Shoppingmall Cart Front :moneybag:

---
+++ frontend/src/pages/Cart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cart">
+    카트 페이지
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  setup() {
+    axios.get("/api/cart/items").then(({data}) =>
+    {
+      console.log(data)
+    })
+
+  }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
