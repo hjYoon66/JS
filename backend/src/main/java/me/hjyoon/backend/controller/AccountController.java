daf97355e2c9d0f388b59b902e7ac3ceb0a57c39
hjYoon66 <poiu601@naver.com>
2024-02-19 09:54:45 +0900

    Gallery Shoppingmall Login Login :heart:

--- backend/src/main/java/me/hjyoon/backend/controller/AccountController.java
+++ backend/src/main/java/me/hjyoon/backend/controller/AccountController.java
@@ -1,11 +1,18 @@
 package me.hjyoon.backend.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import me.hjyoon.backend.entity.Member;
 import me.hjyoon.backend.respository.MemberRepository;
+import me.hjyoon.backend.service.JwtService;
+import me.hjyoon.backend.service.JwtServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
@@ -15,12 +22,22 @@ public class AccountController {
     MemberRepository memberRepository;
 
     @PostMapping("/api/account/login")
-    public int login(@RequestBody Map<String, String> params) {
+    public ResponseEntity login(@RequestBody Map<String, String> params,
+                                HttpServletResponse res) {
         Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
-        if(member != null){
-            return member.getId();
+        if (member != null) {
+            JwtService jwtService = new JwtServiceImpl();
+            int id = member.getId();
+            String token = jwtService.getToken("id", id);
+
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true);
+            cookie.setPath("/");
+
+            res.addCookie(cookie);
+            return new ResponseEntity<>(id, HttpStatus.OK);
         }
-        return 0;
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
 }
