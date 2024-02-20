13b858b5191425b16b339550c3004d4eccc71d22
hjYoon66 <poiu601@naver.com>
2024-02-20 14:22:42 +0900

    TS Project Create :cat:

--- DoItTS/ch02-1/src/utils/makePerson.ts
+++ DoItTS/ch02-1/src/utils/makePerson.ts
@@ -2,5 +2,8 @@ export function makePerson(name: string, age: number){
     return {name: name, age: age}
 }
 export function testMakePerson(){
-    
+    console.log(
+        makePerson('Jane',22),
+        makePerson('Jane',33),
+    )
 }
