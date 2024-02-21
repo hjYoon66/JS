962fbbac8e669faad344d7183ee532b535ad4ccc
hjYoon66 <poiu601@naver.com>
2024-02-21 13:58:52 +0900

    TS Iterator :rabbit:

---
+++ DoItTS/ch06-1/src/createRangeIterator.ts
@@ -0,0 +1,10 @@
+export const createRangeIterator = (from : number, to: number)=>{
+    let currentValue = from
+    return {
+        next(){
+            const value = currentValue < to ? currentValue++ : undefined
+            const done = value == undefined
+            return {value, done}
+        }
+    }
+}
